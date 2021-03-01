a6e0efc786037a58bb78760c3cba989556055664
Osagie Aibangbee <76821049+Beegie01@users.noreply.github.com>
2021-03-01 13:35:01 +0100

    Add files via upload
    Added "save and exit", "continue with password" functionalities

--- rocpapsci.py
+++ rocpapsci.py
@@ -1,43 +1,101 @@
-import imp, string
-imp.load_source('general_app_funcs', 'C:\\Users\\welcome\\Desktop\\MyFuncs\\general_app_funcs.py')
-from general_app_funcs import *
+import sys, string
+
+# adding MyFuncs package to Python's list of lookup paths
+sys.path.append('C:\\Users\\welcome\\Desktop\\MyFuncs')
+
+from fave_app_funcs import *
 
 from rocpapsci_app import RocPapSci
 
 print("\n\n\nWelcome To the ROCK PAPER SCISSORS GAME!!")
 
+# game object for starting afresh
 game = RocPapSci()
 
-START = True
-while START:
+# game object for continuing with saved records
+contd = RocPapSci()
 
-    # collect names
-    prompt = "\nEnter your username below\n>\t"
-    p1, p2 = name_inp(prompt), name_inp(prompt)
+PLAY = True
+while PLAY:
+    # ask to start new game or continue saved game
+    mode = game_mode()
 
-    # assign names
-    game.p1['name'] = p1
-    game.p2['name'] = p2
+    if mode == 'c':
 
-    GAME_ON = True
-    while GAME_ON:
-        # player1 & player 2 start play
-        prompt = f"{p1} RockPaperScissors?\n>\t"
-        game.p1['current_guess'] = game.guess_inp(prompt)
-        prompt = f"{p2} RockPaperScissors?\n>\t"
-        game.p2['current_guess'] = game.guess_inp(prompt)
+        CONTD = True
+        while CONTD:
+            result = contd.retrieve()
 
-        # win check
-        game.win_check(game.p1, game.p2)
+            if result == 'Not Found':
+                CONTD = False
+                continue
 
-        # score display
-        game.scoreboard(game.p1, game.p2)
+            GAME_ON = True
+            while GAME_ON:
+                # player1 & player 2 continue play
+                prompt = f"{contd.p1['username']} \nRock, Paper, Scissors?\n>\t"
+                contd.p1['current_guess'] = contd.guess_inp(prompt)
 
-        ans = ask_next()
+                prompt = f"{contd.p2['username']} \nRock, Paper, Scissors?\n>\t"
+                contd.p2['current_guess'] = contd.guess_inp(prompt)
 
-        if ans in 'yes':
-            continue
+                # win check
+                contd.win_check(contd.p1, contd.p2)
 
-        print("Thanks for playing!")
-        GAME_ON = False
-        START = False
+                # score display
+                contd.scoreboard(contd.p1, contd.p2)
+
+                ans = ask_next()
+
+                if ans in 'yes':
+                    continue
+
+                if ask_to_save():
+                    # save game details before exiting
+                    contd.save_details()
+
+                print("\n\nExiting game...\nThanks for playing!")
+                PLAY, GAME_ON, CONTD = False, False, False
+
+
+    else:
+
+        START = True
+        while START:
+
+            # collect names
+            prompt1 = "\nFirst player, please enter your username below\n>\t"
+            prompt2 = "\nSecond player, please enter your username below\n>\t"
+            inp_name1, inp_name2 = name_inp(prompt1), name_inp(prompt2)
+
+            # assign names
+            game.p1['username'] = inp_name1
+            game.p2['username'] = inp_name2
+
+            GAME_ON = True
+            while GAME_ON:
+                # player1 & player 2 start play
+                prompt = f"{game.p1['username']} Rock, Paper, Scissors?\n>\t"
+                game.p1['current_guess'] = game.guess_inp(prompt)
+
+                prompt = f"{game.p2['username']} Rock, Paper, Scissors?\n>\t"
+                game.p2['current_guess'] = game.guess_inp(prompt)
+
+                # win check
+                game.win_check(game.p1, game.p2)
+
+                # score display
+                game.scoreboard(game.p1, game.p2)
+
+                # ask to continue
+                ans = ask_next()
+
+                if ans in 'yes':
+                    continue
+
+                if ask_to_save():
+                    # save game details before exiting
+                    game.save_details()
+
+                print("\n\nExiting game...\nThanks for playing!")
+                PLAY, GAME_ON, START = False, False, False
--- rocpapsci_app.py
+++ rocpapsci_app.py
@@ -1,14 +1,22 @@
+import sys
+# adding MyFuncs package to Python's list of lookup paths
+sys.path.append('C:\\Users\\welcome\\Desktop\\MyFuncs')
+
+from fave_app_funcs import name_inp, password_inp, ask_to_save
+
+import random
+
+
 class RocPapSci:
 
     def __init__(self):
-        self.p1 = {'name': None, 'current_guess': None, 'games_won': 0, 'games_lost': 0}
-        self.p2 = {'name': None, 'current_guess': None, 'games_won': 0, 'games_lost': 0}
-        self.draws = 0
+        self.p1 = {'username': None, 'current_guess': None, 'games_won': 0, 'games_lost': 0, 'draws': 0}
+        self.p2 = {'username': None, 'current_guess': None, 'games_won': 0, 'games_lost': 0, 'draws': 0}
 
 
     def guess_inp(self, prompt):
 
-        acc_range = 'rock paper scissors'
+        acc_range = ['rock', 'r', 'paper', 'p', 'scissors', 's']
 
         while True:
 
@@ -18,69 +26,163 @@ class RocPapSci:
                 print("Error: Invalid Entry!")
                 continue
 
-            if inp.lower() in 'rock':
+            if inp.lower() in ['r', 'rock']:
                 inp = 'rock'
-            elif inp.lower() in 'scissors':
+            elif inp.lower() in ['s', 'scissors']:
                 inp = 'scissors'
-            elif inp.lower() in 'paper':
+            elif inp.lower() in ['p', 'paper']:
                 inp = 'paper'
 
             return inp.lower()
 
     def win_check(self, p1, p2):
         # player 1 shows rock
-        if self.p1['current_guess'] in 'rock':
+        if self.p1['current_guess'] in ['r', 'rock']:
 
-            if self.p2['current_guess'] in 'scissors':
-                print(f"\n\n{self.p1['name']} WINS!")
+            if self.p2['current_guess'] in ['s', 'scissors']:
+                print(f"\n\n{self.p1['username']} WINS!")
                 self.p1['games_won'] += 1
                 self.p2['games_lost'] += 1
 
-            elif self.p2['current_guess'] in 'paper':
-                print(f"\n\n{self.p2['name']} WINS!")
+            elif self.p2['current_guess'] in ['p', 'paper']:
+                print(f"\n\n{self.p2['username']} WINS!")
                 self.p2['games_won'] += 1
                 self.p1['games_lost'] += 1
 
             else:
                 print("\nSTALEMATE.\n\nNO WINNER!!")
-                self.draws += 1
+                self.p1['draws'] += 1
+                self.p2['draws'] += 1
 
         # player 1 shows paper
-        elif self.p1['current_guess'] in 'paper':
+        elif self.p1['current_guess'] in ['p', 'paper']:
 
-            if self.p2['current_guess'] in 'scissors':
-                print(f"\n\n{self.p2['name']} WINS!")
+            if self.p2['current_guess'] in ['s', 'scissors']:
+                print(f"\n\n{self.p2['username']} WINS!")
                 self.p2['games_won'] += 1
                 self.p1['games_lost'] += 1
 
-            elif self.p2['current_guess'] in 'rock':
-                print(f"\n\n{self.p1['name']} WINS!")
+            elif self.p2['current_guess'] in ['r', 'rock']:
+                print(f"\n\n{self.p1['username']} WINS!")
                 self.p1['games_won'] += 1
                 self.p2['games_lost'] += 1
 
             else:
                 print("\nSTALEMATE.\n\nNO WINNER!!")
-                self.draws += 1
+                self.p1['draws'] += 1
+                self.p2['draws'] += 1
 
         #player 1 shows a scissors
-        elif self.p1['current_guess'] in 'scissors':
+        elif self.p1['current_guess'] in ['s', 'scissors']:
 
-            if self.p2['current_guess'] in 'paper':
-                print(f"\n\n{self.p1['name']} WINS!")
+            if self.p2['current_guess'] in ['p', 'paper']:
+                print(f"\n\n{self.p1['username']} WINS!")
                 self.p1['games_won'] += 1
                 self.p2['games_lost'] += 1
 
-            elif self.p2['current_guess'] in 'rock':
-                print(f"\n\n{self.p2['name']} WINS!")
+            elif self.p2['current_guess'] in ['r', 'rock']:
+                print(f"\n\n{self.p2['username']} WINS!")
                 self.p2['games_won'] += 1
                 self.p1['games_lost'] += 1
 
             else:
                 print("\nSTALEMATE.\n\nNO WINNER!!")
-                self.draws += 1
+                self.p1['draws'] += 1
+                self.p2['draws'] += 1
 
     def scoreboard(self, p1, p2):
 
-        print(f"\n\n{self.p1['name']}\n\tGAMES WON: {self.p1['games_won']}")
-        print(f"\n\n{self.p2['name']}\n\tGAMES WON: {self.p2['games_won']}")
-        print(f"\n\nGAMES DRAWN: {self.draws}")
+        print(f"\n\n{self.p1['username']}\n\tGAMES WON: {self.p1['games_won']}")
+        print(f"\n\n{self.p2['username']}\n\tGAMES WON: {self.p2['games_won']}")
+        print(f"\n\nGAMES DRAWN: {self.p1['draws']}")
+
+    def save_details(self):
+
+        SEP1, SEP2 = random.choice(['*', '-', '@', '&', '!']), random.choice(['%', '>', '<', '/', '|'])
+
+        password = random.choice('abcdefgsh')+SEP1+str(random.randint(0,2000))+SEP2+random.choice('zywxvjknmpr')
+
+        hand = open('C:\\Users\\welcome\\Desktop\\SimplePythonChallenges\\RockPaperScissors\\game_data.txt', 'a')
+
+        # stored fields include
+        # {password: [[username1, games_won, games_lost, games_drawn], [username2, games_won, games_lost, games_drawn]]}
+        info = f"\n{dict( [ (password, [[self.p1['username'], self.p1['games_won'], self.p1['games_lost'], self.p1['draws'] ], [self.p2['username'], self.p2['games_won'], self.p2['games_lost'], self.p2['draws'] ]] )])}"
+
+        hand.write(info)
+
+        hand.close()
+
+        print(f"\n\nGame saved!\nTo continue as {self.p1['username']} and {self.p2['username']} \nUse the password given below:\n{password}")
+
+    def retrieve(self):
+
+        # ask for the saved usernames
+        prompt = "\nEnter password:\n>\t"
+        pword = password_inp(prompt)
+
+
+        # retrieve stored information from file
+        hand = open('C:\\Users\\welcome\\Desktop\\SimplePythonChallenges\\RockPaperScissors\\game_data.txt')
+
+        # file data contains a list of dictionary pairs
+        file_data = hand.read().strip()
+
+        # declaring useful array variables
+        passwords = {}
+        draws = []
+        usernames = []
+        wins = []
+        losses = []
+
+
+        # for each dictionary list in the file
+        for n, dic in enumerate(file_data.split("\n")):
+            # eliminating the first line containing the comment on column order
+            if n == 0:
+                continue
+
+            # transform the dict saved in str format back into dict type
+            di = eval(dic)
+
+            # here, each key is a unique password
+            # value is a list of list containing each player's records
+            for k,v in di.items():
+
+                # collecting and indexing the passwords from the file
+                if k == pword:
+                    print("Password Found!")
+
+                    # here value is the list of lists containing each player's records
+                    for each_player_rec in v:
+                        # segmenting the information onto separate lists
+                        usernames.append(each_player_rec[0]), wins.append(each_player_rec[1]), losses.append(each_player_rec[2]), draws.append(each_player_rec[3])
+
+        # when username was collected
+        if len(usernames) < 1:
+            print(f"Password: {pword} is not on record\nPlease check for incorrect spelling")
+            return "Not Found"
+
+        else:
+            # after match has been found
+            # print(f"Users: {usernames}\nWins: {wins}\nLosses: {losses}")
+            self.p1['username'], self.p2['username'] = usernames[0], usernames[1]
+            self.p1['games_won'], self.p2['games_won'] = wins[0], wins[1]
+            self.p1['games_lost'], self.p2['games_lost'] = losses[0], losses[1]
+            self.p1['draws'], self.p2['draws'] = draws[0], draws[1]
+
+            print("\n\nPlayers' records have been retrieved and restored!")
+            return 'Done'
+
+def game_mode():
+
+    acc_range = ['new', 'n', 'c', 'cont']
+    while True:
+        # ask to start new or continue saved game
+        prompt = "To start new game, enter 'new'\nTo continue saved game, enter 'cont'\n>\t"
+        inp = input(prompt)
+
+        if inp[0].lower() not in acc_range:
+            print("\nPlease select 'new' or 'cont'!")
+            continue
+
+        return inp[0].lower()
