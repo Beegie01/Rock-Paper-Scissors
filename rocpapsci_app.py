03d4ec1a79ac1deeabb4e3dc0ab9b9884b493557
Osagie Aibangbee <76821049+Beegie01@users.noreply.github.com>
2021-03-04 20:09:33 +0100

    Add files via upload

--- rocpapsci_app.py
+++ rocpapsci_app.py
@@ -1,3 +1,7 @@
+import sys
+# adding MyFuncs package to Python's list of lookup paths
+sys.path.append('C:\\Users\\welcome\\Desktop\\MyFuncs')
+
 from fave_app_funcs import name_inp, password_inp, ask_to_save
 
 import random
